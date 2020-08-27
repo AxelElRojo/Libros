998735966e502c7bd4b0af4f1961f328f4238a2d
AxelElRojo <axelzkai.123@gmail.com>
2020-08-27 13:09:02 -0500

    Extrajé carga en una función

--- main.cpp
+++ main.cpp
@@ -2,25 +2,19 @@
 #include <cstring>
 int main(int argc, char* argv[]){
     cLista libros;
-    if(!libros.cargar()){
-        cout << "ADVERTENCIA: No se pudo encontrar el archivo de base de datos!\n"
-             << "Si es la primera vez que usa el programa, asegúrese de guardar al salir.\n";
-        pausa();
-    }
     if(argc > 1){
         std::vector<cLibro>resultados;
-        int res;
         if(argv[1][0] == '-')
             switch (argv[1][1]){
                 case 'a':
+                    carga(libros);
                     libros.buscarAutor(resultados, argv[2]);
-                    res = resultados.size();
-                    cout << "Hubo un total de " << res << " resultados: ";
+                    mostrarResultados(resultados);
                 break;
                 case 't':
+                    carga(libros);
                     libros.buscarTitulo(resultados, argv[2]);
-                    res = resultados.size();
-                    cout << "Hubo un total de " << res << " resultados: ";
+                    mostrarResultados(resultados);
                 break;
                 case 'h':
                 cout << "AYUDA:\n\t-a Búsqueda por autor\n\t-t Búsqueda por título\n\t-h Esta pantalla\n"
@@ -36,6 +30,7 @@ int main(int argc, char* argv[]){
             cout << "Opción inválida!\nPrueba -h o RTFM\n";
     }else{
         short opc;
+        carga(libros);
         do{
             opc = menu();
             switch(opc){
